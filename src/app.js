4a2790bb358d275f65c0880b7abb46ca96c27a06
Kalpit Agrawal <kalpitagrawal06@gmail.com>
2026-03-16 01:39:57 +0530

    feat: add utility code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,43 @@
 import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 
 const app = express()
+
+
+
+
+
+
+
+
+
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+
+
+
+
+
+
+
+// proper error response
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        success: false,
+        message: err.message,
+        errors: err.errors
+    });
+});
 
 export { app }
